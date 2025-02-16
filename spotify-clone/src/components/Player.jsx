8a9db40168dc2cc570bd4dd9dd4ee23ab596715e
Lucas Pessoal <lucas-marques2003@hotmail.com>
2025-02-16 04:31:12 -0300

    ✨ Add Song and Player components; implement song display and playback controls

---
+++ spotify-clone/src/components/Player.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import {
+    faBackwardStep,
+    faCirclePlay,
+    faForwardStep,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
+
+const Player = () => {
+    return (
+        <div className="player">
+            <div className="player__controllers">
+                <Link to="/song/2">
+                    <FontAwesomeIcon
+                        className="player__icon"
+                        icon={faBackwardStep}
+                    />
+                </Link>
+
+                <FontAwesomeIcon
+                    className="player__icon player__icon--play"
+                    icon={faCirclePlay}
+                />
+
+                <Link to="/song/3">
+                    <FontAwesomeIcon
+                        className="player__icon"
+                        icon={faForwardStep}
+                    />
+                </Link>
+            </div>
+
+            <div className="player__progress">
+                <p className="player__time">00:00</p>
+
+                <div className="player__bar">
+                    <div className="player__bar-progress"></div>
+                </div>
+
+                <p className="player__time">02:00</p>
+            </div>
+        </div>
+    );
+};
+
+export default Player;
