9d7241be338d68982c224ebb0ebff613a2920212
Lucas Pessoal <lucas-marques2003@hotmail.com>
2025-02-16 04:02:59 -0300

    🚧 Add SongList and SongItem components; integrate into Artist page for displaying popular songs

--- spotify-clone/src/pages/Artist.jsx
+++ spotify-clone/src/pages/Artist.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { faCirclePlay } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
+import SongList from "../components/SongList";
 
 const Artist = () => {
-    return <div>Artist</div>;
+    return (
+        <div className="artist">
+            <div
+                className="artist__header"
+                style={{
+                    backgroundImage:
+                        "linear-gradient(to bottom, var(--_shade), var(--_shade)), url(https://i.scdn.co/image/ab67618600001016909a162796dbc3629a1fadeb)",
+                }}
+            >
+                <h2 className="artist__title">Jorge & Mateus</h2>
+            </div>
+
+            <div className="artist__body">
+                <h2>Populares</h2>
+
+                <SongList />
+            </div>
+
+            <Link to={`/song/1`}>
+                <FontAwesomeIcon
+                    className="single-item__icon single-item__icon--artist"
+                    icon={faCirclePlay}
+                />
+            </Link>
+        </div>
+    );
 };
 
 export default Artist;
