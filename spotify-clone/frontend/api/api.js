b4468b7250019a674d6f743e4e214904a09172b1
Lucas Pessoal <lucas-marques2003@hotmail.com>
2025-02-17 11:12:39 -0300

    🐛 Fix process env

--- spotify-clone/frontend/api/api.js
+++ spotify-clone/frontend/api/api.js
@@ -1,8 +1,9 @@
 import "dotenv/config";
 import axios from "axios";
 
-const { NODE_ENV } = process.env;
-const URL = NODE_ENV === "development" ? "http://localhost:3000/api" : "/api";
+// const { NODE_ENV } = process.env;
+// const URL = "http://localhost:3000/api";
+const URL = "https://spotify-replication.onrender.com/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
